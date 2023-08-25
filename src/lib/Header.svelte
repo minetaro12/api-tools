1bfc0ff17554d0405497094e3278eb8b333f0722
minetaro12 <osawa2003@gmail.com>
2023-08-25 23:35:42 +0900

    ソースへのリンクを追加

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,16 @@
-<header class="text-white bg-gray-800 p-4">
+<header class="text-white bg-gray-800 p-4 flex justify-between items-center">
   <h1>
     <a class="text-2xl" href="/">API Tools</a>
   </h1>
+  <nav>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/minetaro12/api-tools"
+          target="_blank"
+          rel="noopener noreferrer">GitHub</a
+        >
+      </li>
+    </ul>
+  </nav>
 </header>
