9078f4230fbcfe699e7d237f13e5944bf286f2a0
minetaro12 <osawa2003@gmail.com>
2023-08-26 12:20:03 +0900

    UIの改善

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,16 +1,12 @@
-<header class="text-white bg-gray-800 p-4 flex justify-between items-center">
+<script>
+  import Menu from "./Menu.svelte";
+</script>
+
+<header
+  class="text-white bg-gray-800 p-4 flex justify-between items-center sticky top-0"
+>
   <h1>
     <a class="text-2xl" href="/">API Tools</a>
   </h1>
-  <nav>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/minetaro12/api-tools"
-          target="_blank"
-          rel="noopener noreferrer">GitHub</a
-        >
-      </li>
-    </ul>
-  </nav>
+  <Menu />
 </header>
---
+++ src/lib/Menu.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let isOpen = false;
+</script>
+
+<!-- ボタン -->
+<input
+  type="button"
+  value="Menu"
+  on:click={() => (isOpen = !isOpen)}
+  class="bg-gray-800 border-2 border-gray-300 px-2 py-1 rounded cursor-pointer hover:bg-gray-700"
+/>
+
+<!-- メニュー部分 -->
+{#if isOpen}
+  <div
+    class="popup fixed top-14 right-4 bg-white text-black border-2 border-gray-300y px-3 py-2 rounded"
+  >
+    <nav>
+      <ul class="[&>li]:m-2">
+        <li>
+          <a
+            href="https://tool.0sn.net"
+            target="_blank"
+            rel="noopener noreferrer">DNS Tools</a
+          >
+        </li>
+        <li>
+          <a
+            href="https://github.com/minetaro12/api-tools"
+            target="_blank"
+            rel="noopener noreferrer">GitHub</a
+          >
+        </li>
+      </ul>
+    </nav>
+  </div>
+{/if}
