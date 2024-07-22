23dcd2ffa61b443242392b1a5e5accba40f9d4d9
minetaro12 <osawa2003@gmail.com>
2024-07-22 19:31:29 +0900

    メニューの動作を改善

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,37 +1,41 @@
 <script lang="ts">
   export let isOpen = false;
+
+  const close = (event: Event) => {
+    if (event.target === event.currentTarget) {
+      isOpen = false;
+    }
+  };
 </script>
 
 <!-- ボタン -->
-<input
-  type="button"
-  value="Menu"
+<button
   on:click={() => (isOpen = !isOpen)}
-  class="bg-gray-800 border-2 border-gray-300 px-2 py-1 rounded cursor-pointer hover:bg-gray-700"
-/>
+  class="bg-gray-700 border-gray-300 px-3 py-2 rounded hover:bg-gray-500"
+>
+  Menu</button
+>
 
 <!-- メニュー部分 -->
-{#if isOpen}
-  <div
-    class="popup fixed top-14 right-4 bg-white text-black border-2 border-gray-300y px-3 py-2 rounded"
-  >
-    <nav>
-      <ul class="[&>li]:m-2">
-        <li>
-          <a
-            href="https://tool.0sn.net"
-            target="_blank"
-            rel="noopener noreferrer">DNS Tools</a
-          >
-        </li>
-        <li>
-          <a
-            href="https://github.com/minetaro12/api-tools"
-            target="_blank"
-            rel="noopener noreferrer">GitHub</a
-          >
-        </li>
-      </ul>
-    </nav>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class:hidden={!isOpen}
+  class="fixed flex flex-row-reverse w-full h-full bg-black bg-opacity-30 top-0 left-0"
+  on:click={close}
+>
+  <div class="flex flex-col bg-gray-700 py-10 font-semibold text-xl shadow-2xl">
+    <a
+      href="https://tool.0sn.net"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="px-7 py-3 hover:bg-gray-500">DNS Tools</a
+    >
+    <a
+      href="https://github.com/minetaro12/api-tools"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="px-7 py-3 hover:bg-gray-500">GitHub</a
+    >
   </div>
-{/if}
+</div>
